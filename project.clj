1611b7d600cbacba911c49392f6ecdc51bc1c8c9
Ray Willig <ray.willig@gmail.com>
2014-07-08 18:52:31 -0400

    shipshape for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject ring-middleware-index-file "1.0.5-SNAPSHOT"
+(defproject org.clojars.raywillig/ring-middleware-index-file "1.0.7"
   :description "Ring Middleware for serving up an index file."
+  :url "https://github.com/rwillig/ring-middleware-index-file"
   :lein-release {:deploy-via :clojars}
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm     {:name "git "
+            :url  "https://github.com/rwillig/ring-middleware-index-file"}          
   :dependencies [
     [org.clojure/clojure "1.5.1"]
     [ring "1.2.0"]])
